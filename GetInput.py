22316b448f2168930552c15f3664b3a60475676f
Nitin <ntipun@gmail.com>
2018-02-03 15:41:07 -0500

    food

---
+++ GetInput.py
@@ -0,0 +1,12 @@
+#Combines formula and Food Search
+
+import formula
+
+continueprogram = input("Press enter to continue.")
+if len(continueprogram) >= 0:
+    print()
+    import FoodSearch
+
+
+
+
--- formula.py
+++ formula.py
@@ -17,25 +17,25 @@ def kcal_maintain(BMR, activity):
     elif activity == "5":
         return (float(BMR) * 1.9)
 
-def AskBMI():
-    print ("We will now calculate the amount of calories required to maintain your weight.")
-    sex = input("Are you male (enter 'm') or female (enter 'f')?")
-    sex.lower()
-    if sex != 'm' and sex!= 'f':
-        print ("Invalid user input.")
-    weight = input('What is your weight, in pounds?')
-    height = input('What is your height, in inches?')
-    age = input('What is your age, in years?')
-    BMR = BMR_calc(weight, height, age, sex)
-    return BMR
+print ("We will now calculate the amount of calories required to maintain your weight.")
+sex = input("Are you male (enter 'm') or female (enter 'f')?")
+sex.lower()
+if sex != 'm' and sex!= 'f':
+    print ("Invalid user input.")
+weight = input('What is your weight, in pounds?')
+height = input("What is your height, in inches?")
+age = input('What is your age, in years?')
+BMR = BMR_calc(weight, height, age, sex)
 
 
 activity = input('On a scale of 1 to 5, how active are you?')
+print()
 
-x = kcal_maintain(AskBMI,activity)
+x = kcal_maintain(BMR,activity)
 
-print ("In order to lose one pound per week, you smhould consume %f Calories per day" % (x-500))
+print ("In order to lose one pound per week, you should consume %f Calories per day" % (x-500))
 print ("In order to lose half a pound per week, you should consume %f Calories per day" % (x-250))
 print ("In order to maintain your weight, you should consume %f Calories per day" % x)
 print ("In order to gain half a pound per week, you should consume %f Calories per day" % (x+250))
 print ("In order to gain one pound per week, you should consue %f Calories per day" % (x+500))
+print()
