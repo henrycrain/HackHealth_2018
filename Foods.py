48f9ffa36d90664c93117904c4e6129c67073142
Nitin <ntipun@gmail.com>
2018-02-03 13:06:46 -0500

    food

---
+++ Foods.py
@@ -0,0 +1,16 @@
+# dictionary = { food_name : [Calories, Fat, Sodium, Carbs, Protein] }
+
+foods = {"PizzaSlice" : [275, 10, 550, 34, 12], "Cheeseburger" : [460, 22, 570, 31, 37],
+         "Apple" : [80, 0.25, 0, 22, 0.5], "Steak" : [679, 48, 146, 34, 12],
+         "Orange" : [61, 0.2, 0, 15, 1.2], "ChickenBreast" : [142, 3.1, 64, 0, 26.7],
+         "Salad" : [100, 9, 360, 3, 2], "PotatoChips" : [154, 10, 136, 14, 2],
+         "Pasta" : [600, 8, 960, 108, 20], "Soda" : [150, 0, 30, 41, 0],
+         "Cookies" : [120, 6, 126, 15, 2], "FrenchFries" : [421, 22, 389, 52, 5],
+         "WhiteRice" : [206, 0.4, 2, 45, 4], "Banana" : [105, 0.4, 1, 27, 1],
+         "Hamburger" : [352, 17, 497, 29, 20], "SkimMilk" : [90, 0, 130, 13, 8]
+         "WholeMilk" : [150, 8, 125, 12, 8], "Omelette" : [94, 7, 95, 0.4, 6],
+         "ChickenNoodleSoup" : [87, 2.9, 343, 8, 6], "OrangeJuice" : [111, 0.5, 2, 26, 1.7],
+         "PB&JSandwich" : [189, 8, 227, 26, 3], "Yogurt" : [100, 0.7, 61, 6, 17],
+         "Taco" : [170, 10, 290, 12, 8], "ChickenWings" : [367, 22, 1070, 6, 34],
+         "Grapes" : [62, 0.3, 2, 16, 0.6], "Fish"  : [366, 22, 109, 0, 39],
+         "BrownRice" : [216, 2, 10, 45, 5], "ChocolateBar" : [120, 6.5, 2, 14, 1.3]}
