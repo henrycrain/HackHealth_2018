b3595fc40e8799f53174e59ba993b88a32237747
Brandon Tai <byt1998@gmail.com>
2018-02-03 14:49:27 -0500

    proper comparisons

---
+++ maxrecintake.py
@@ -0,0 +1,13 @@
+import formula
+
+max_fat = 65 * formula.x / 2000
+#grams
+
+max_sodium = 2400 * formula.x / 2000
+#mg
+
+max_carbs = 300 * formula.x / 2000
+#grams
+
+max_protein = 50 * formula.x / 2000
+#grams
